48a4584783833b6557285d2b1e9c81a9e986c33d
Michael Brown <114196048+NarrowSpeedster@users.noreply.github.com>
2023-09-19 19:40:59 -0700

    Creating circles of rgb colors for fun.

---
+++ CFF/PlayingWithColors.py
@@ -0,0 +1,21 @@
+import colorsys
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+t.speed(0)
+n = 70
+h = 0
+for I in range (360):
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
+    h+= 1/n
+    t.color(c)
+    t.left(1)
+    t.fd(1)
+    for j in range (2):
+        t.left(2)
+        t.circle(10)
+        t.circle(20)
+        t.circle(30)
+        t.circle(40)
+        t.circle(50)
