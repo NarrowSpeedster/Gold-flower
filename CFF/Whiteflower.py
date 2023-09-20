947d9ec01fbb386043b8c47fbe309c01d0db1dc8
Michael Brown <114196048+NarrowSpeedster@users.noreply.github.com>
2023-09-19 19:31:22 -0700

    Red Circle with Black background.

---
+++ CFF/Whiteflower.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import *
+win=Screen()
+win.bgcolor("Black")
+
+old=Turtle()
+old.color("white")
+old.speed(10)
+old.begin_fill()
+old.color("red")
+old.penup()
+old.setposition(10,-400)
+old.pendown()
+old.circle(350)
+old.end_fill()
+old.begin_fill()
+old.color("white")
